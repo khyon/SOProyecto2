c2e1e4c54646e611b5dd015023bf4d8f8ff4c2bc
Andrés <andresulloa97@gmail.com>
2017-04-05 00:05:12 -0500

    Add files via upload

---
+++ GestionMemoria/src/Memoria/GestionRAM.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Memoria;
+
+import UnidadesDeMemoria.Particiones;
+import UnidadesDeMemoria.Proceso;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * @author Andres
+ */
+public abstract class GestionRAM {
+    protected ArrayList<Particiones> RAM = new ArrayList();
+    protected int tamaño_total;
+    protected int etiqueta = 1;
+    protected int kernel;
+    protected Queue<Proceso> ColaEspera = new LinkedList<Proceso>();
+    
+     public int calcula_localidad(int i) {
+        int localidad=0;
+        for(int j=0;j<i; j++){
+            localidad+=RAM.get(j).getTamaño();
+        }
+        return localidad;
+    }
+      public void imprimir() {
+        for (int i = 1; i < RAM.size(); i++) {
+            System.out.println(RAM.get(i));
+        }
+    }
+
+    public ArrayList getRAM() {
+        return RAM;
+    }
+
+    public void setRAM(ArrayList RAM) {
+        this.RAM = RAM;
+    }
+
+    public Queue getColaEspera() {
+        return ColaEspera;
+    }
+
+    public void setColaEspera(Queue ColaEspera) {
+        this.ColaEspera = ColaEspera;
+    }
+   
+    
+}
---
+++ GestionMemoria/src/UnidadesDeMemoria/Particiones.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UnidadesDeMemoria;
+
+import UnidadesDeMemoria.Proceso;
+
+/**
+ *
+ * @author Andres
+ */
+public class Particiones {
+ public boolean estado;
+ protected int tamaño;
+ protected int localidad;
+ protected int orden;
+ protected int id;
+ protected int proceso;
+ protected int ajuste;
+ 
+    public Particiones(boolean estado, int tamaño, int localidad) {
+        this.estado = estado;
+        this.tamaño = tamaño;
+        this.localidad = localidad;
+        this.orden = orden;
+    }
+     
+      public Particiones(boolean estado, int tamaño, int localidad, int orden, int id) {
+        this.estado = estado;
+        this.tamaño = tamaño;
+        this.localidad = localidad;
+        this.orden = orden;
+        this.id=id;
+    }
+      public Particiones(boolean estado, int tamaño, int localidad, int orden, int id, int proceso) {
+        this.estado = estado;
+        this.tamaño = tamaño;
+        this.localidad = localidad;
+        this.orden = orden;
+        this.proceso=proceso;
+        
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public int getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(int tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public int getAjuste() {
+        return ajuste;
+    }
+
+    public void setAjuste(int ajuste) {
+        this.ajuste = ajuste;
+    }
+
+    public int getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(int localidad) {
+        this.localidad = localidad;
+    }
+
+    public int getOrden() {
+        return orden;
+    }
+
+    public void setOrden(int orden) {
+        this.orden = orden;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProceso() {
+        return proceso;
+    }
+
+    public void setProceso(int proceso) {
+        this.proceso = proceso;
+    }
+
+    @Override
+    public String toString(){
+        if(estado==false){
+            return  " \n ID particion =" + id + 
+                "\n Estado =" + estado + 
+                 "\n Localidad =" + localidad + 
+                "\n Tamaño =" + tamaño +
+                "\n Orden =" + orden; 
+                }
+       else{   
+               return "\n ID particion =" + id + 
+                "\n Estado =" + estado + 
+                "\n Localidad =" + localidad +        
+                "\n Tamaño =" + tamaño + 
+                "\n Proceso =" +"P"+proceso;
+       
+    }}
+   
+}
